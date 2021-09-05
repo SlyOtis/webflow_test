c729d3a80e497712aca8c34514e6b3c9c7644f90
Martin Ottersland <Ottini93@gmail.com>
2021-09-05 18:00:08 +0200

    Bumped html vs

--- dist/player.js
+++ dist/player.js
@@ -1,5 +1,5 @@
 (function () {
-  fetch('https://cdn.jsdelivr.net/gh/SlyOtis/webflow_test@1.0.4/dist/index.html').then(res => {
+  fetch('https://cdn.jsdelivr.net/gh/SlyOtis/webflow_test@1.0.8/dist/index.html').then(res => {
     console.log('Fetch success')
     return res.text()
   }).then(content => {
