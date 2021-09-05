4e61cfcf0b244b702168104f789bb17a02651643
Martin Ottersland <Ottini93@gmail.com>
2021-09-05 18:24:08 +0200

    Added hash feedback

--- dist/player.js
+++ dist/player.js
@@ -3,6 +3,7 @@
     console.log('Fetch success')
     return res.text()
   }).then(content => {
+    console.log(window.location.hash)
     const parent = document.querySelector(".player")
     if (parent) {
       parent.innerHTML = content
