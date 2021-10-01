7d7b56037f951eff98a802fd8324e1c1ed9ab296
Martin Ottersland <ottini93@gmail.com>
2021-10-01 23:27:32 +0200

    feat: added player split for embeded support

---
+++ upload/src/components/Loading.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+</script>
+
+<main>
+	<h1>Loading...</h1>
+</main>
+
+<style>
+	main {
+		position: relative;
+		width: auto;
+		height: auto;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: white;
+	}
+</style>
